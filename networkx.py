69cf4b71fffb3d6648833822a1a3eac495d57734
jendralhxr <jendral.hxr@gmail.com>
2024-03-07 12:31:58 +0700

    trying out superpixel methods

--- networkx.py
+++ networkx.py
@@ -1,39 +1,36 @@
-# -*- coding: utf-8 -*-
-
-import networkx as nx
-import matplotlib.pyplot as plt
-
-# Create a graph
-G = nx.Graph()
-
-# Add nodes with attributes
-G.add_node(1, label='A', color='red', weight=1, text='wololo', val=2)
-G.add_node(2, label='B', color='blue', weight=2 , text='kwiwi', val=3)
-G.add_node(3, label='C', color='green', weight=0.5, text='lululu', val=4)
-
-# Add edges
-G.add_edge(1, 2)
-G.add_edge(1, 3)
-G.add_edge(2, 3)
-
-# Access node attributes
-#print("Node 1 attributes:", G.nodes[1])
-#print("Label of node 2:", G.nodes[2]['label'])
-
-# Draw the graph with node attributes
-node_labels = {node: G.nodes[node]['label'] for node in G.nodes()}
-node_colors = [G.nodes[node]['color'] for node in G.nodes()]
-node_weights = [G.nodes[node]['weight'] * 1000 for node in G.nodes()]  # Scale weights for better visibility
-
-nx.draw(G, with_labels=True, labels=node_labels, node_color=node_colors, node_size=node_weights, edge_color='black')
-
-# Display the graph
-plt.title("Graph with Node Attributes")
-plt.show()
-
-
-""" 
-"""
+#
+# import networkx as nx
+# import matplotlib.pyplot as plt
+# 
+# # Create a graph
+# G = nx.Graph()
+# 
+# # Add nodes with attributes
+# G.add_node(1, label='A', color='red', weight=1, text='wololo', val=2)
+# G.add_node(2, label='B', color='blue', weight=2 , text='kwiwi', val=3)
+# G.add_node(3, label='C', color='green', weight=0.5, text='lululu', val=4)
+# 
+# # Add edges
+# G.add_edge(1, 2)
+# G.add_edge(1, 3)
+# G.add_edge(2, 3)
+# 
+# # Access node attributes
+# #print("Node 1 attributes:", G.nodes[1])
+# #print("Label of node 2:", G.nodes[2]['label'])
+# 
+# # Draw the graph with node attributes
+# node_labels = {node: G.nodes[node]['label'] for node in G.nodes()}
+# node_colors = [G.nodes[node]['color'] for node in G.nodes()]
+# node_weights = [G.nodes[node]['weight'] * 1000 for node in G.nodes()]  # Scale weights for better visibility
+# 
+# nx.draw(G, with_labels=True, labels=nod-e_labels, node_color=node_colors, node_size=node_weights, edge_color='black')
+# 
+# # Display the graph
+# plt.title("Graph with Node Attributes")
+# plt.show()
+# 
+# 
 
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -42,19 +39,22 @@ import matplotlib.pyplot as plt
 G = nx.Graph()
 
 # Add nodes
-G.add_nodes_from([1, 2, 3, 4, 5])
+G.add_nodes_from([1, 2, 3, 4, 5, 6, 7])
 
 # Add edges with weights
 G.add_edge(1, 2, weight=1)
-G.add_edge(1, 3, weight=2)
-G.add_edge(2, 3, weight=2)
-G.add_edge(2, 4, weight=1)
-G.add_edge(3, 5, weight=3)
-G.add_edge(4, 5, weight=2)
+G.add_edge(1, 5, weight=1)
+G.add_edge(2, 3, weight=1)
+G.add_edge(3, 4, weight=1)
+G.add_edge(4, 5, weight=1)
+G.add_edge(4, 6, weight=1)
+G.add_edge(5, 6, weight=1)
+G.add_edge(6, 7, weight=1)
+
 
 # Find the shortest path between nodes 1 and 5
-shortest_path = nx.shortest_path(G, source=1, target=5, weight='weight')
-shortest_path_length = nx.shortest_path_length(G, source=1, target=5, weight='weight')
+shortest_path = nx.shortest_path(G, source=1, target=7, weight='weight')
+shortest_path_length = nx.shortest_path_length(G, source=1, target=7, weight='weight')
 
 print("Shortest path:", shortest_path)
 print("Shortest path length:", shortest_path_length)
--- slic.py
+++ slic.py
@@ -13,36 +13,55 @@ image_gray= cv.cvtColor(cv.bitwise_not(image), cv.COLOR_BGR2GRAY)
 ret, image_thr = cv.threshold(image_gray, 0, 120, cv.THRESH_OTSU) # other thresholding method may also work
 render = cv.cvtColor(image_thr, cv.COLOR_GRAY2BGR)
 
-space= 4
+space= 3
 key= 32;
 
-slic = cv.ximgproc.createSuperpixelSLIC(image_thr,algorithm = cv.ximgproc.SLICO, region_size = space)
-slic.iterate()
+# Define SEEDS algorithm parameters
+num_superpixels = 5000
+num_levels = 4
+prior = 2
+num_histogram_bins = 5
+double_step = False
+
+slic = cv.ximgproc.createSuperpixelSEEDS(image_thr.shape[1], image_thr.shape[0], 1, num_superpixels, num_levels, prior, num_histogram_bins, double_step)
+slic.iterate(image_thr, num_iterations=4)
+
+#slic = cv.ximgproc.createSuperpixelSLIC(image_thr,algorithm = cv.ximgproc.SLICO, region_size = space)
+#slic = cv.ximgproc.createSuperpixelSLIC(image_thr,algorithm = cv.ximgproc.SLIC, region_size = space)
+#slic = cv.ximgproc.createSuperpixelSLIC(image_thr,algorithm = cv.ximgproc.MSLIC, region_size = space)
+
+#slic = cv.ximgproc.createSuperpixelLSC(image_thr, region_size = space)
+
+
+
+#slic.iterate()
 mask= slic.getLabelContourMask()
 num_slic = slic.getNumberOfSuperpixels()
 lbls = slic.getLabels()
 
+print(num_slic)
 render = cv.cvtColor(image_thr, cv.COLOR_GRAY2BGR)
+
 for cls_lbl in range(num_slic):
     fst_cls = np.argwhere(lbls==cls_lbl)
     x, y = fst_cls[:, 0], fst_cls[:, 1]
     c = (x.mean(), y.mean())
-    cx= int(c[1])
-    cy= int(c[0])
-    if (image_thr.item(cy,cx) != 0):
-        render.itemset((cy,cx,1), 255)
-        #print(f'{cls_lbl} point at: ({int(c[1])}, {int(c[0])})')
-    else:
-        render.itemset((cy,cx,2), 255)
-        #print(f'{cls_lbl} void at: ({int(c[1])}, {int(c[0])})')
+    print(f'{lbls}:{c}')
+    # cx= int(c[1])
+    # cy= int(c[0])
+    # if (image_thr.item(cy,cx) != 0):
+    #     render.itemset((cy,cx,1), 255)
+    #     print(f'{cls_lbl} point at: ({int(c[1])}, {int(c[0])})')
+#     # else:
+#     #     render.itemset((cy,cx,2), 255)
+#     #     print(f'{cls_lbl} void at: ({int(c[1])}, {int(c[0])})')
     
-#render = cv.cvtColor(image_thr, cv.COLOR_GRAY2BGR)
-
+render = cv.cvtColor(image_thr, cv.COLOR_GRAY2BGR)
 mask2 = cv.cvtColor(mask, cv.COLOR_GRAY2BGR)
-#render= cv.bitwise_or(render, mask2)
-#cv.imshow("show", render)
+render= cv.bitwise_or(render, mask2)
+cv.imshow("show", render)
 cv.imwrite(sys.argv[2], render)
 print(f'save to: {sys.argv[2]}')
 #cv.imshow("mask", mask)
-#key = cv.waitKey(0) & 0xff
+key = cv.waitKey(0) & 0xff
 

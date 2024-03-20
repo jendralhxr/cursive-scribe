a6c751f51e203d51e19474ec7b732028a86b487a
yoen <jendral.hxr@gmail.com>
2024-03-20 14:35:58 +0700

    O(n^2) search

--- slic.py
+++ slic.py
@@ -56,7 +56,7 @@ ret, cue = cv.threshold(image_gray, 0, 120, cv.THRESH_OTSU) # other thresholding
 render = cv.cvtColor(cue, cv.COLOR_GRAY2BGR)
 
 # LSC and SLIC 
-space= 3
+SPACE= 3
 key= 32;
 
 # SEEDS parameters
@@ -69,10 +69,10 @@ double_step = False
 #slic = cv.ximgproc.createSuperpixelSEEDS(cue.shape[1], cue.shape[0], 1, num_superpixels, num_levels, prior, num_histogram_bins, double_step)
 #slic.iterate(cue, num_iterations=4)
 
-slic = cv.ximgproc.createSuperpixelSLIC(cue,algorithm = cv.ximgproc.SLICO, region_size = space)
-#slic = cv.ximgproc.createSuperpixelSLIC(cue,algorithm = cv.ximgproc.SLIC, region_size = space)
-#slic = cv.ximgproc.createSuperpixelSLIC(cue,algorithm = cv.ximgproc.MSLIC, region_size = space)
-#slic = cv.ximgproc.createSuperpixelLSC(cue, region_size = space)
+slic = cv.ximgproc.createSuperpixelSLIC(cue,algorithm = cv.ximgproc.SLICO, region_size = SPACE)
+#slic = cv.ximgproc.createSuperpixelSLIC(cue,algorithm = cv.ximgproc.SLIC, region_size = SPACE)
+#slic = cv.ximgproc.createSuperpixelSLIC(cue,algorithm = cv.ximgproc.MSLIC, region_size = SPACE)
+#slic = cv.ximgproc.createSuperpixelLSC(cue, region_size = SPACE)
 slic.iterate()
 
 #mask= slic.getLabelContourMask()
@@ -93,73 +93,103 @@ scribe= nx.Graph() # start anew, just in case
 isi=0
 # valid superpixel
 for n in range(num_slic):
-    if ( len(moments[n])>3): # remove spurious superpixel with area less than 2 px 
-        #cx= int(moments[n][:,0][1])
+    if ( len(moments[n])>SPACE): # remove spurious superpixel with area less than 2 px 
+        #cx= int(moments[n][:,0][1]) # first elem
         #cy= int(moments[n][:,1][1])
-        #render.itemset((cy,cx,0), 255) # first elem
-        #cx= int(moments[n][:,0][-1])
+        #render.itemset((cy,cx,0), 255) 
+        #cx= int(moments[n][:,0][-1]) # last elem
         #cy= int(moments[n][:,1][-1])
-        #render.itemset((cy,cx,1), 255) # centroid
-        cx= int( np.mean(moments[n][1:,0]) )
+        #render.itemset((cy,cx,2), 255) 
+        cx= int( np.mean(moments[n][1:,0]) ) # centroid
         cy= int( np.mean(moments[n][1:,1]) )
-        render.itemset((cy,cx,2), 255) # last elem
+        render.itemset((cy,cx,1), 255) 
         scribe.add_node(int(isi), label=int(lbls[cy,cx]), area=(len(moments[n])-1), pos=(cx,-cy) )
         #print(f'point{n} at ({cx},{cy})')
         isi= isi+1
 
-# establish edges from the shortest distance between nodes
-scribe.remove_edges_from(scribe.edges) # start anew, just in case
 temp= nx.get_node_attributes(scribe, 'pos')
 cx=[]
 cy=[]
 for key, value in temp.items():
     cx.append(value[0])
     cy.append(-value[1])
+
+scribe.remove_edges_from(scribe.edges) # start anew, just in case
+distance = np.full(isi, 1e6)
+dest = np.full(isi, -1, dtype=int)
+distance_ud = np.full(isi, 1e6)
+dest_ud = np.full(isi, -1, dtype=int)
+
+# establish edges from the shortest distance between nodes, forward check
 for m in range(isi):
-    distance= 1e3 # distance to closest neighboring keypoints
-    distance_ud= 1e3 # distance to closer vertical neighbor 
-    orig= 0
-    dest= 0
-    orig_ud= 0
-    dest_ud= 0
     # the search
-    for n in range(m+1, isi):
+    for n in range(isi):
         # find shortest distance
-        vane= freeman(cx[m]-cx[n], cy[m]-cy[n])
-        tdist= math.sqrt( math.pow(cx[m]-cx[n],2) + math.pow(cy[m]-cy[n],2) )
-        if (tdist<distance):
-            orig= m
-            dest= n
-            distance= tdist
-        if (tdist<distance_ud) and ((vane==2) or (vane==6)):
-            orig_ud= m
-            dest_ud= n
-            distance_ud= tdist
-            #print(f'edge between {orig} and {dest}')
+        if (m!=n):
+            vane= freeman(cx[m]-cx[n], cy[m]-cy[n])
+            tdist= math.sqrt( math.pow(cx[m]-cx[n],2) + math.pow(cy[m]-cy[n],2) )
+            if (tdist<distance[m]) and (dest[n]!=m):
+                dest[m]= n
+                distance[m]= tdist
+            if (tdist<distance_ud[m]) and (dest_ud[n]!=m) and ((vane==2) or (vane==6)):
+                dest_ud[m]= n
+                distance_ud[m]= tdist
     # the assignment
-    # establish the edges if not already exist between closest a pair of nodes
-    if (scribe.has_edge(orig,dest)==False) and (scribe.has_edge(dest,orig)==False) and (orig!=dest): # 1 px smear in the main stroke 
-        if     (cue.item( int((cy[dest]+cy[orig])/2)    ,int((cx[dest]+cx[orig])/2))    !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2+1)  ,int((cx[dest]+cx[orig])/2))    !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2+1)  ,int((cx[dest]+cx[orig])/2+1))  !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2)    ,int((cx[dest]+cx[orig])/2+1))  !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2-1)  ,int((cx[dest]+cx[orig])/2+1))  !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2-1)  ,int((cx[dest]+cx[orig])/2))    !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2-1)  ,int((cx[dest]+cx[orig])/2-1))  !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2)    ,int((cx[dest]+cx[orig])/2-1))  !=0) \
-            or (cue.item( int((cy[dest]+cy[orig])/2+1)  ,int((cx[dest]+cx[orig])/2-1))  !=0):
-            fill='#00FF00' # green in-stroke, RGB
-            scribe.add_edge(orig, dest, color=fill, weight=1e1/distance, code=vane)
-            #print(f'stroke between {orig} and {dest}')
-        else:
-            # jumps over the void
-            # additional check for 2 or 6 freeman code (straight up- and downward)
-            fill='#0000FF' # blue void: article, harakat, or tashkeel RGB
-            scribe.add_edge(orig_ud, dest_ud, color=fill, weight=1e1/distance_ud, code=vane)
-            #print(f'tashkeel between {orig} and {dest}')
+    if (dest[m]!=m):
+        midx= int((cx[dest[m]]+cx[m])/2)
+        midy= int((cy[dest[m]]+cy[m])/2)
+        kernel=0
+        if (cue.item( midy  ,midx  ) !=0):
+            kernel= kernel+1
+        if (cue.item( midy+1,midx  ) !=0):
+           kernel= kernel+1
+        if (cue.item( midy+1,midx+1) !=0):
+            kernel= kernel+1
+        if (cue.item( midy  ,midx+1) !=0):
+            kernel= kernel+1
+        if (cue.item( midy-1,midx+1) !=0):
+            kernel= kernel+1
+        if (cue.item( midy-1,midx  ) !=0):
+            kernel= kernel+1
+        if (cue.item( midy-1,midx-1) !=0):
+           kernel= kernel+1
+        if (cue.item( midy  ,midx-1) !=0):
+            kernel= kernel+1
+        if (cue.item( midy+1,midx-1) !=0):
+            kernel= kernel+1
             
+        midx= int((cx[dest_ud[m]]+cx[m])/2)
+        midy= int((cy[dest_ud[m]]+cy[m])/2)
+        kernel_ud=0
+        if (cue.item( midy  ,midx  ) !=0):
+            kernel_ud= kernel_ud+1
+        if (cue.item( midy+1,midx  ) !=0):
+           kernel_ud= kernel_ud+1
+        if (cue.item( midy+1,midx+1) !=0):
+            kernel_ud= kernel_ud+1
+        if (cue.item( midy  ,midx+1) !=0):
+            kernel_ud= kernel_ud+1
+        if (cue.item( midy-1,midx+1) !=0):
+            kernel_ud= kernel_ud+1
+        if (cue.item( midy-1,midx  ) !=0):
+            kernel_ud= kernel_ud+1
+        if (cue.item( midy-1,midx-1) !=0):
+           kernel_ud= kernel_ud+1
+        if (cue.item( midy  ,midx-1) !=0):
+            kernel_ud= kernel_ud+1
+        if (cue.item( midy+1,midx-1) !=0):
+            kernel_ud= kernel_ud+1    
+        print(f'{m}: {dest[m]}/{distance[m]}:{kernel} {dest_ud[m]}/{distance_ud[m]}:{kernel_ud}')        
+        
+        scribe.add_edge(m, dest_ud[m], color='#0000FF', weight=1e1/distance_ud[m]/2, code=vane)
+        if (kernel>2):
+            scribe.add_edge(m, dest[m], color='#00FF00', weight=1e1/distance[m], code=vane)
+        if (kernel_ud>2):
+            scribe.add_edge(m, dest_ud[m], color='#00FF00', weight=1e1/distance[m], code=vane)
         
         
+#scribe.number_of_edges()            
+
 # re-fetch the attributes from drawing
 # nodes
 positions = nx.get_node_attributes(scribe,'pos')
@@ -173,10 +203,10 @@ nx.draw(scribe,
         pos=positions, 
         with_labels=True, node_color='orange',
         node_size=area*25,
+        font_size=8,
         # edges' param
         edge_color=colors, 
-        width=weights*4,
-        font_size=8
+        width=weights*2,
         )
 
 # save graph object to file

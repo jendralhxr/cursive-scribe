30a97c5c377b5082a496687473d7055aff749af6
jendralhxr <jendral.hxr@gmail.com>
2024-04-30 17:34:11 +0700

    gonna search-automate the rotate

--- spacehist.py
+++ spacehist.py
@@ -16,8 +16,11 @@ image=  cv2.bitwise_not(image)
 
 height= image.shape[0]
 width= image.shape[1]
+center=(width/2, height/2)
 
+CHANNEL= 2
 image_gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_gray= image[:,:,CHANNEL]
 _, thresholded = cv2.threshold(image_gray, 0, 1, cv2.THRESH_OTSU)
 
 # Calculate histogram
@@ -26,9 +29,27 @@ row_indices= np.arange(height)
 histogram_x = np.sum(thresholded, axis=0)  # Sum along columns to get histogram
 histogram_y = np.sum(thresholded, axis=1)  # Sum along columns to get histogram
 
-
 SLIC_SPACE= 3
 phi= 1.6180339887498948482 # ppl says this is a beautiful number :)
+
+# line segments
+plt.figure(dpi=300)
+render= thresholded.copy()
+#render= cv2.cvtColor(render, cv2.COLOR_BGR2RGB)
+render= cv2.cvtColor(render[:,:,CHANNEL], cv2.COLOR_GRAY2RGB)
+plt.imshow(render) 
+plt.plot(histogram_y, row_indices)
+#plt.title('Histogram of Grayscale Image After Otsu Thresholding Along X-Axis')
+plt.xlabel('Row Index')
+plt.ylabel('count')
+plt.show()
+
+# rotate
+DEGREE= 1
+M = cv2.getRotationMatrix2D(center, DEGREE, 1.0)
+dst = cv2.warpAffine(thresholded, M, (width,height))
+integral = np.trapz(histogram_x)
+
 
 # spaces, horizontal
 render= image.copy()
@@ -50,14 +71,4 @@ plt.xlabel('Column Index')
 plt.ylabel('Number of Pixels')
 plt.show()
 
-# 
-plt.figure(dpi=300)
-render= image.copy()
-#render= cv2.cvtColor(render, cv2.COLOR_BGR2RGB)
-render= cv2.cvtColor(render[:,:,2], cv2.COLOR_GRAY2RGB)
-plt.imshow(render) 
-plt.plot(histogram_y, row_indices)
-#plt.title('Histogram of Grayscale Image After Otsu Thresholding Along X-Axis')
-plt.xlabel('Row Index')
-plt.ylabel('count')
-plt.show()
+

0b7513f0aa320d17b38e8074c22453e97d0ff63d
jendralhxr <jendral.hxr@gmail.com>
2024-05-02 11:49:09 +0700

    detect orientation w/ vertical projection hist

--- spacehist.py
+++ spacehist.py
@@ -5,23 +5,48 @@ Created on Sat Apr 27 10:35:01 2024
 @author: rdx
 """
 
-import cv2
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Load the image in grayscale
 filename= 'sifatline.png'
-image = cv2.imread(filename, cv2.IMREAD_COLOR)
-image=  cv2.bitwise_not(image)
+image = cv.imread(filename, cv.IMREAD_COLOR)
+image=  cv.bitwise_not(image)
 
 height= image.shape[0]
 width= image.shape[1]
 center=(width/2, height/2)
 
 CHANNEL= 2
-image_gray= cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image_gray= cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 image_gray= image[:,:,CHANNEL]
-_, thresholded = cv2.threshold(image_gray, 0, 1, cv2.THRESH_OTSU)
+#_, thresholded = cv.threshold(image_gray, 0, 1, cv.THRESH_OTSU)
+_, thresholded = cv.threshold(image_gray, 0, 1, cv.THRESH_TRIANGLE)
+
+def rottrap(angle):
+    M = cv.getRotationMatrix2D(center, angle, 1.0)
+    dst = cv.warpAffine(thresholded, M, (width,height))
+    hist= np.sum(dst, axis=1)  # Sum along columns to get histogram
+    return np.trapz(hist)
+
+# find optimal orientation
+left, right = -1, 1  # Define the range to search within
+epsilon = 1e-6  # Define the desired precision
+while abs(right - left) > epsilon:
+    mid1 = left + (right - left) / 3
+    mid2 = right - (right - left) / 3
+    if rottrap(mid1) < rottrap(mid2):
+        right = mid2
+    else:
+        left = mid1
+# found it
+min_x = (left + right) / 2
+#print("Minimum value:", min_value)
+#print("Corresponding x:", min_x)
+#min_value = rottrap(min_x)
+M = cv.getRotationMatrix2D(center, min_x, 1.0)
+thresholded = cv.warpAffine(thresholded, M, (width,height))
 
 # Calculate histogram
 column_indices = np.arange(width)  # Generate array of column indices
@@ -34,9 +59,11 @@ phi= 1.6180339887498948482 # ppl says this is a beautiful number :)
 
 # line segments
 plt.figure(dpi=300)
-render= thresholded.copy()
-#render= cv2.cvtColor(render, cv2.COLOR_BGR2RGB)
-render= cv2.cvtColor(render[:,:,CHANNEL], cv2.COLOR_GRAY2RGB)
+#render= thresholded.copy()
+#render= cv.cvtColor(render, cv.COLOR_BGR2RGB)
+#render= cv.cvtColor(render[:,:,CHANNEL], cv.COLOR_GRAY2RGB)
+_, render = cv.threshold(thresholded, 0, 240, cv.THRESH_BINARY)
+render= cv.cvtColor(render, cv.COLOR_GRAY2RGB)
 plt.imshow(render) 
 plt.plot(histogram_y, row_indices)
 #plt.title('Histogram of Grayscale Image After Otsu Thresholding Along X-Axis')
@@ -44,11 +71,11 @@ plt.xlabel('Row Index')
 plt.ylabel('count')
 plt.show()
 
-# rotate
-DEGREE= 1
-M = cv2.getRotationMatrix2D(center, DEGREE, 1.0)
-dst = cv2.warpAffine(thresholded, M, (width,height))
-integral = np.trapz(histogram_x)
+# gonna crop-select each lines here
+#
+#
+#
+
 
 
 # spaces, horizontal
@@ -61,7 +88,7 @@ for i in range(width-1, -1, -1):
         for j in range(height):
             render.itemset((j,i,2), 80)
     
-render= cv2.cvtColor(render, cv2.COLOR_BGR2RGB)
+render= cv.cvtColor(render, cv.COLOR_BGR2RGB)
 plt.figure(dpi=300)
 plt.imshow(render) 
 # Plot the histogram

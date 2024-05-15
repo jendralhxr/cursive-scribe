bc077eb9fd59309a88252e9248d4bb3ba20a37e3
yoen <jendral.hxr@gmail.com>
2024-05-15 10:12:15 +0700

    parsing the rasm connected components

--- features.py
+++ features.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue May  7 11:39:58 2024
@@ -9,7 +10,7 @@ import os
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-os.chdir("D:")
+os.chdir("/shm")
 import networkx as nx
 
 plt.figure(dpi=300)
@@ -42,10 +43,10 @@ _, gray = cv.threshold(image_gray, 0, 80, cv.THRESH_OTSU) # less smear
 #_, gray= cv.threshold(image_gray, 0, 1, cv.THRESH_TRIANGLE)
 
 
-# ORB
-orb = cv.ORB_create()
-keypoints, descriptors = orb.detectAndCompute(gray, None)
-image_with_keypoints = cv.drawKeypoints(gray, keypoints, None)
+# # ORB
+# orb = cv.ORB_create()
+# keypoints, descriptors = orb.detectAndCompute(gray, None)
+# image_with_keypoints = cv.drawKeypoints(gray, keypoints, None)
 
 def sharpen(img):
     kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
@@ -55,14 +56,14 @@ def sharpen(img):
 sharp1= sharpen(gray)
 sharp2= sharpen(sharp1)
 
-# SIFT
-sift = cv.SIFT_create()
-kp0, descriptors = sift.detectAndCompute(gray, None)
-ks0 = cv.drawKeypoints(gray, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-kp1, descriptors = sift.detectAndCompute(sharp1, None)
-ks1 = cv.drawKeypoints(gray, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-kp2, descriptors = sift.detectAndCompute(sharp2, None)
-ks2 = cv.drawKeypoints(gray, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# # SIFT
+# sift = cv.SIFT_create()
+# kp0, descriptors = sift.detectAndCompute(gray, None)
+# ks0 = cv.drawKeypoints(gray, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# kp1, descriptors = sift.detectAndCompute(sharp1, None)
+# ks1 = cv.drawKeypoints(gray, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# kp2, descriptors = sift.detectAndCompute(sharp2, None)
+# ks2 = cv.drawKeypoints(gray, keypoints, None, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 cue= gray.copy()
 render = cv.cvtColor(gray, cv.COLOR_GRAY2BGR)
@@ -87,6 +88,8 @@ for j in range(height):
         else:
             moments_void[lbls[j,i]] = np.append(moments_void[lbls[j,i]], np.array([[i,j]]), axis=0)
 
+# draw render here
+# draw2(renders)
 # generating nodes
 scribe= nx.Graph() # start anew, just in case
 
@@ -105,16 +108,16 @@ for n in range(num_slic):
 # connected components
 from dataclasses import dataclass, field
 from typing import List
+from typing import Optional
 
 @dataclass
 class ConnectedComponents:
-    bgn_x: int
-    bgn_y: int
-    end_x: int
-    end_y: int
-    mid_x: int
-    mid_y: int
+    rect: (int,int,int,int) # from bounding rectangle
+    centroid: (int,int) # centroid moment
     nodes: List[int] = field(default_factory=list)
+    mat: Optional[np.ndarray] = field(default=None, repr=False)
+
+components=[]
 
 # component = ConnectedComponents(bgn_x=0, bgn_y=0, end_x=100, end_y=100, mid_x=50, mid_y=50, nodes=[2])
 
@@ -125,9 +128,29 @@ for n in range(scribe.number_of_nodes()):
     ccv= gray.copy()
     cv.floodFill(ccv, None, seed, 200, loDiff=(5), upDiff=(5))
     _, ccv = cv.threshold(ccv, 100, 200, cv.THRESH_BINARY)
-    #draw2(ccv)
-    cv.imwrite(str(n)+'-'+str(pos[n][0])+'-'+str(pos[n][1])+'.png', ccv)
-    # check
-    # append
+    mu= cv.moments(ccv)
+    if mu['m00'] > SLIC_SPACE*PHI:
+        mc= (int(mu['m10'] / (mu['m00'])), int(mu['m01'] / (mu['m00'])))
+        box= cv.boundingRect(ccv)
+        #print(f'keypoint[{n}] at ({mc[0]},{mc[1]})')
+        # append keypoint if the component already exists
+        found=0
+        for i in range(len(components)):
+            if components[i].centroid==mc:
+                components[i].nodes.append(n)
+                found=1
+                break
+        if (found==0):
+            components.append(ConnectedComponents(box, mc))
+            components[len(components)-1].nodes.append(n)
+            components[len(components)-1].mat = ccv.copy()
+
+components.sort(key=lambda item:item.centroid[0], reverse=True)
+for i in range(len(components)):
+    print(f'{i} at {components[i].centroid}')
+    rasm= components[i].mat[\
+        components[i].rect[1]:components[i].rect[1]+components[i].rect[3],\
+        components[i].rect[0]:components[i].rect[0]+components[i].rect[2]]
+    cv.imwrite(str(i)+'.png', rasm)
     
         

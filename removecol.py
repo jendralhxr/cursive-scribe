fbf8cf772de4d9add95f50865ab2a5934c6fe342
yoen <jendral.hxr@gmail.com>
2024-05-20 19:34:42 +0700

    argv to be precise

--- removecol.py
+++ removecol.py
@@ -8,21 +8,22 @@ Created on Mon May 20 09:10:33 2024
 
 from PIL import Image
 import numpy as np
+import sys
 
 # Step 1: Open the image
-image_path = 'p15.png'
+image_path = sys.argv[1]
 image = Image.open(image_path)
 
 # Step 2: Convert the image to a NumPy array
 image_array = np.array(image)
 
 # Step 3: Remove a single column
-column_to_remove = 222  # For example, remove the 50th column
+column_to_remove = int(sys.argv[3])  # For example, remove the 50th column
 image_array = np.delete(image_array, column_to_remove, axis=1)
 
 # Step 4: Convert the modified array back to an image
 modified_image = Image.fromarray(image_array)
 
 # Step 5: Save or display the modified image
-modified_image.save('p15new.png')
+modified_image.save(sys.argv[2])
 modified_image.show()

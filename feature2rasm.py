6639d82875395aef9058f18929c2dac0cfe9f5d5
jendralhxr <jendral.hxr@gmail.com>
2024-11-21 11:26:34 +0700

    needs to be more careful while copy-pasting

--- feature2rasm.py
+++ feature2rasm.py
@@ -815,7 +815,7 @@ def find_histogram_min(img, ANGLE):
     
     return projection_hist_smoothed, valleys
 
-# 1ed projection histogram for segmenting the strokes
+# slanted projection histogram for segmenting the strokes
 SLANT1= 0
 SLANT2= 3.1415 / pow(PHI,4)
 
@@ -825,8 +825,8 @@ for n in range(len(components)):
         seed= pos[components[n].nodes[0]]
         cv.floodFill(ccv, None, seed, (STROKEVAL, THREVAL, THREVAL), loDiff=(5), upDiff=(5))
 
-hist1, valleys1= find_histogram_min(ccv, SLANT1)
-hist2, valleys2= find_histogram_min(ccv, SLANT2)
+hist1, valleys1= find_histogram_min(ccv, SLANT1) # red
+hist2, valleys2= find_histogram_min(ccv, SLANT2) # green
 
 # plt.plot(hist1, color='red', label="projection angle "+f"{SLANT1:.4f}"+" rad")  
 # plt.plot(hist2, color='green', label="projection angle "+f"{SLANT2:.4f}"+" rad")  
@@ -869,11 +869,11 @@ for x_start in valleys1:
                             scribe_dia.nodes[transition_node]['color']='#10F010'
                     
 for x_start in valleys2:
-    x_end= x_start- math.tan(SLANT1) * ccv.shape[0]
+    x_end= x_start- math.tan(SLANT2) * ccv.shape[0]
     if (x_end>=0):
         active_stroke= False
         for y_pos in range(ccv.shape[0]):
-            x_pos= int (x_start - math.tan(SLANT1)*y_pos)
+            x_pos= int (x_start - math.tan(SLANT2)*y_pos)
             if y_pos<ccv.shape[0] and x_pos<ccv.shape[1]:
                 if ccv[y_pos][x_pos][0] == STROKEVAL:
                     ccv[y_pos][x_pos][1] = FOCUSVAL
@@ -889,6 +889,7 @@ for x_start in valleys2:
                         transition_node= find_closest_node(scribe_dia, midpoint[0], midpoint[1])
                         if scribe_dia.nodes[transition_node]['rasm']==True:
                             scribe_dia.nodes[transition_node]['color']='#10A010'
+
 draw(ccv)
 
 ###### graph construction from line image ends here

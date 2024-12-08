a0a5977afe03762aa89b1db77256954a9fdd1053
jendralhxr <jendral.hxr@gmail.com>
2024-12-08 13:24:38 +0700

    extracted pages

--- view2page.py
+++ view2page.py
@@ -24,7 +24,6 @@ image = cv.imread(filename)
 height= image.shape[0]
 width= image.shape[1]
 
-
 image_gray= image[:,:,2]
 _, image_binary = cv.threshold(image_gray, 0, THREVAL, cv.THRESH_OTSU) # less smear
 column_indices = np.arange(width)  
@@ -33,15 +32,15 @@ row_indices = np.arange(height)
 histogram_x = np.sum(image_binary, axis=0)/THREVAL  # Sum along columns
 diff_abs = np.abs ( np.diff(histogram_x))
 
-edges = np.where(diff_abs > height/pow(PHI,k))[0]
-image_bgr= image.copy()
+edges = np.where(diff_abs > height/pow(PHI,k+1))[0]
+# image_bgr= image.copy()
 # for x in valleys1[0]:
 #     cv.line(image_bgr, (x, 0), (x, image_bgr.shape[0]), (0, 0, 255), 8)  # Red line
 # for x in edges:
 #     cv.line(image_bgr, (x, 0), (x, image_bgr.shape[0]), (0, 255, 0), 8)  # green line
 # draw(image_bgr)
 
-threshold = SLIC_SPACE  # Adjust based on expected cluster proximity
+threshold = pow(PHI,2)*SLIC_SPACE  # Adjust based on expected cluster proximity
 clusters = [[edges[0]]]
 
 for value in edges[1:]:
@@ -50,6 +49,7 @@ for value in edges[1:]:
     else:
         clusters.append([value])
 cluster_centers_x = [np.mean(cluster) for cluster in clusters]
+[np.mean(cluster) for cluster in clusters]
 
 for n in range(1, len(cluster_centers_x)):
     pagecrop= image_binary[:,int(cluster_centers_x[-(n+1)]):int(cluster_centers_x[-n])]
